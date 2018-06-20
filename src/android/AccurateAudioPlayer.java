69af50e822baa2eb586ae5235a4063fb19a4cd2d
mikael-onsilva <mikael.onsilva@gmail.com>
2018-06-20 12:20:36 -0300

    Add files via upload

--- src/android/AccurateAudioPlayer.java
+++ src/android/AccurateAudioPlayer.java
@@ -323,7 +323,7 @@ public class AccurateAudioPlayer implements OnCompletionListener, OnPreparedList
               this.player.start();
               this.setState(STATE.MEDIA_RUNNING);
               this.seekOnPrepared = 0; //insures this is always reset
-              callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "teste"));
+              this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "teste"));
           } else {
               this.prepareOnly = false;
           }
